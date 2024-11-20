21adf2d0c3d8c4a91bd32923f13e2d82ee38a1a6
AmmanRizwan <ammanrizwan00007@gmail.com>
2024-11-20 17:31:34 +0530

    create a controller and router

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const router = require('./routes/movie.routes.js');
 require('dotenv').config();
 const port = process.env.PORT;
-require('./configs/movie.config'); 
+require('./configs/movie.config');
 app.use(cors());
 app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
-require('./routes/Author.route')(app); // here to add you route directory 
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/movies', router);
 app.listen(port, () => console.log(`Listening on port: ${port}`));
