b0e0788235f7996fe56c271e4d9c51c6bd6e089f
AmmanRizwan <ammanrizwan00007@gmail.com>
2024-11-20 13:14:34 +0530

    create a modules and make a movie.model.js

---
+++ Server/modules/movie.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const MovieSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please Enter the Title"],
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    actors: {
+      type: Array,
+      required: true,
+    },
+    categories: {
+      type: Array,
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Movie = mongoose.model('movie', MovieSchema);
+
+module.exports = Movie;
