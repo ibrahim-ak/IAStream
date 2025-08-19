c7c5c9a246f43368fb35ba44733a005d0d4b0946
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-08-19 18:10:05 +0530

    fix: update database connection URI and restructure server initialization

--- Server/configs/db.js
+++ Server/configs/db.js
@@ -4,7 +4,7 @@ const username = env.DB_USERNAME;
 const pw = env.DB_PASSWORD;
 const DB = env.DB_NAME;
 // to connect whith data base
-const uri = `mongodb+srv://${username}:${pw}@mongodb1.5w1gh.mongodb.net/${DB}?retryWrites=true&w=majority&appName=mongodb1`; 
+const uri = `mongodb+srv://${username}:${pw}@iastreamdb.yi3hnzj.mongodb.net/${DB}?retryWrites=true&w=majority&appName=IAStreamDB`; 
 mongoose.connect(uri)
      .then(() => console.log("Established a connection to the database"))
      .catch(err => console.log("Something went wrong when connecting to the database", err));
