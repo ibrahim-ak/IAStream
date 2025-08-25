921c788050463aee3cda8c27a98aafe6ae89a859
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-08-25 22:11:34 +0530

    feat: implement OTP verification process and enhance user registration flow

--- Server/controller/auth/signup.js
+++ Server/controller/auth/signup.js
@@ -5,9 +5,13 @@ const { hashPassword } = require("../../utils/bcrypt.js");
 const { createToken } = require("../../utils/jwt.js");
 const generateOTP = require("../../utils/generate-otp.js");
 const sendMail = require("../../services/stmp.js");
+const mongoose = require("mongoose");
 
 const register = async (req, res, next) => {
+  const transaction = await mongoose.startSession();
   try {
+    transaction.startTransaction();
+
     const { name, email, phone, password } = req.body;
 
     const existUser = await User.findOne({ email: email });
@@ -23,16 +27,20 @@ const register = async (req, res, next) => {
     if (!user) {
       return throwCustomError(400, "Something went wrong! Cannot create user");
     }
-
-    const token = createToken(user._id);
     
     const otp_string = generateOTP();
   
-    const otp = await OTP.create({ user_id: user._id, otp: otp_string });
+    const otp = await OTP.create({ 
+      user_id: user._id, 
+      otp: otp_string,
+      expireAt: new Date(Date.now() + 1000 * 60 * 10) // 10 minutes from now
+    });
 
     if (!otp) {
       return throwCustomError(400, "Something went wrong! OTP is not created");
     }
+
+    await transaction.commitTransaction();
 
     await sendMail(
       user.email, 
@@ -45,11 +53,74 @@ const register = async (req, res, next) => {
       }
     );
 
-    res.status(201).json({ message: "SignUp Successfully!", data: token});
+    res.status(201).json({ message: "SignUp is successfully! Please check your email for verification",});
   }
   catch (err) {
+    transaction.abortTransaction();
     next(err);
+  } finally {
+    transaction.endSession();
   }
 }
 
-module.exports = register;
+const verifyOTP = async (req, res, next) => {
+  const transaction = await mongoose.startSession();
+  try {
+    transaction.startTransaction();
+
+    const { email, otp } = req.body;
+
+    if (!email) {
+      return throwCustomError(400, "Email is required");
+    }
+
+    if (!otp) {
+      return throwCustomError(400, "OTP is required");
+    }
+
+    const validOTP = await OTP.findOne({ otp });
+
+    if (!validOTP) {
+      return throwCustomError(400, "Invalid OTP");
+    }
+
+    if (validOTP.expireAt < new Date()) {
+      return throwCustomError(400, "OTP expires");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return throwCustomError(404, "Email record not found!");
+    }
+
+    if (user.isVerify) {
+      return throwCustomError(400, "Email already verified");
+    }
+
+    user.isVerify = true;
+    await user.save();
+
+    const deleteOTP = await OTP.findOneAndDelete({ otp });
+
+    if (!deleteOTP) {
+      return throwCustomError(400, "Something went wrong! Cannot delete the otp");
+    }
+
+    transaction.commitTransaction();
+
+    const token = createToken(user._id);
+
+    await sendMail(user.email, "welcome", "Welcome to IAStream", {email: user.email});
+
+    res.status(200).json({ message: "Email Verified!", data: token });
+  }
+  catch (err) {
+    transaction.abortTransaction();
+    next(err);
+  } finally {
+    transaction.endSession();
+  }
+}
+
+module.exports = {register, verifyOTP};
