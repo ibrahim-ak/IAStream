bc8bcaf075d883b911a186f7f6ab60f5c19ac27c
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-08-28 11:57:19 +0530

    feat: add validation schema to signup, movie creation, and review creation routes

--- Server/routes/auth/index.js
+++ Server/routes/auth/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { checkSchema } = require("express-validator");
 
 const { register, verifyOTP } = require("../../controller/auth/signup.js");
 const signin = require("../../controller/auth/login.js");
@@ -8,11 +9,12 @@ const {
   forgotPassword,
   resendOTP,
 } = require("../../controller/auth/forgot.js");
+const checkAuthSchema = require("../../validation/auth/index.js");
 
 const router = express.Router({ mergeParams: true });
 
 router.route("/signin").post(signin);
-router.route("/signup").post(register);
+router.route("/signup").post(checkSchema(checkAuthSchema) ,register);
 router.route("/verify-otp").post(verifyOTP);
 router.route("/email-verify").post(checkEmail);
 router.route("/forgot-otp").post(sendVerifyOTP);
--- Server/routes/movie/index.js
+++ Server/routes/movie/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
+const { checkSchema } = require("express-validator");
 
 const postMovie = require("../../controller/movie/create");
 const getMovies = require("../../controller/movie/get");
 const getMovie = require("../../controller/movie/getbyid");
 const deleteMovie = require("../../controller/movie/delete");
 const updateMovie = require("../../controller/movie/update");
+const createMovieSchema = require("../../validation/movie/index");
 
 const router = express.Router({ mergeParams: true });
 
-router.route("/user/:userId/movies/").get(getMovies).post(postMovie);
+router.route("/user/:userId/movies/").get(getMovies).post(checkSchema(createMovieSchema), postMovie);
 
 router.route("/user/:userId/movie/:id").get(getMovie).put(updateMovie).delete(deleteMovie);
 
