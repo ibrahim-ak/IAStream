193a6c641cb45a88d988eb80d4411da6d1796dc5
Amman Rizwan <97797756+AmmanRizwan@users.noreply.github.com>
2025-08-29 18:19:56 +0530

    test: enhance JWT token tests for validity and verification; add tests for invalid tokens

--- Server/__test__/unit/utils/create-token.test.js
+++ Server/__test__/unit/utils/create-token.test.js
@@ -5,8 +5,27 @@ const userMocks = require("../../mocks/user");
 describe("Create a JWT Token Generation", () => {
   const user = userMocks[0];
   const payload = { id: user._id, name: user.name, email: user.email, role: user.role };
+
   it("should generate a valid JWT token", () => {
     const token = createToken(payload);
     expect(token).toBeDefined();
+  });
+
+  it("should generate a valid token which is string type", () => {
+    const token = createToken(payload);
+    expect(typeof token).toBe("string");
   })
+
+  // sign created at same secret that will return the same token
+  it("should return token for each encrypted token", () => {
+    const token1 = createToken(payload);
+    const token2 = createToken(payload);
+
+    expect(token1).toEqual(token2);
+  })
+
+  it("should create a token", () => {
+    const token = createToken(payload);
+    expect(token).not.toBe("secret");
+  });
 })
