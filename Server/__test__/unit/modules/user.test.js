016e38cfacb78376ad5c5983a8c768b3db8daf3a
Amman Rizwan <97797756+AmmanRizwan@users.noreply.github.com>
2025-08-29 19:40:20 +0530

    feat: add Docker configuration and initial test setup; include .dockerignore, Dockerfile, and docker-compose files

---
+++ Server/__test__/unit/modules/user.test.js
@@ -0,0 +1,8 @@
+const { describe, it } = require("@jest/globals");
+const User = require("../../../modules/user");
+
+describe("Create a new User for testing", () => {
+  it("", () => {
+    
+  })
+})
